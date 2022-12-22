e324e74854fde5672f5a8425ae52299a9bcf1b32
David Gidony <david.gidony@gmail.com>
2022-12-22 14:16:29 +0200

    added vpc options

--- main.tf
+++ main.tf
@@ -8,13 +8,12 @@ module "vpc" {
   azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
-
+  
+  enable_dns_hostnames = true
   enable_ipv6 = true
-
   enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
-
   enable_vpn_gateway = false
 
   vpc_tags = {
