894c1e31786929f8c1a0e9056ba864220afe6559
David Gidony <david.gidony@gmail.com>
2022-12-22 11:35:18 +0200

    1st commit

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
 variable "vpc_name" {
   description = "The Name of the VPC."
   type        = string
