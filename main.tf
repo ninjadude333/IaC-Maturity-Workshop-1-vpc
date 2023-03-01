236c3fc6c24b65fac0b80c3cb00d4919a0309ed6
David Gidony <david.gidony@gmail.com>
2023-03-01 09:08:45 +0200

    tag update

--- main.tf
+++ main.tf
@@ -5,21 +5,16 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  azs             = [local.vpc_azs]
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
   
-  enable_dns_hostnames = true
-  enable_ipv6 = true
+  enable_dns_hostnames   = true
+  enable_ipv6            = true
   enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
-  enable_vpn_gateway = false
+  enable_vpn_gateway     = false
 
-  vpc_tags = {
-    Owner     = "David Gidony"
-    Env       = "Maturity-Workshop"
-    Terraform = "True"
-    Phase     = var.Phase
-  }
+  vpc_tags = local.tags
 }
